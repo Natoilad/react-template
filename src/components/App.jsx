43e6f5eb1adfbdaf4819450ba2c7a2acf940754e
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-04-24 22:18:42 +0300

    css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ export const App = () => {
   return (
     <div
       style={{
+        backgroundColor: 'black',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: 'white',
       }}
     >
-      React homework template
+      React template
     </div>
   );
 };
